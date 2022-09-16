902ea42c4b91a8e30ab908976633066d823966b8
고동현 <dhjk35@naver.com>
2022-09-16 13:54:28 +0900

    Chore: svg 사용을 위해 @svgr/webpack 설치 및 필요한 환경 설정

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  // 피그마에서 잘못된 마스크가 넘어올 경우 이를 해결
+                  removeRasterImages: false,
+                  removeStyleElement: false,
+                  removeUnknownsAndDefaults: false,
+                  // svgr이 svg의 크기를 수정할 수 있도록 변경
+                  removeViewBox: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
