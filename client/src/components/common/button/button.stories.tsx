69778e32b4ebd3b2f717dac88ddc774751cb81e5
고동현 <dhjk35@naver.com>
2022-09-25 16:46:39 +0900

    Feat: 버튼 컴포넌트 구현
    - 버튼 관련 필요한 static files 추가
    - dynamic component naming을 이용해 렌더링될 버튼을 선택할 수 있게 구현
    - rippleAnimation을 위해 클릭 이벤트가 필요하지만, onClick 이벤트를
      다른 곳에 사용할 수 있도록 onMouseDown을 통해 적용
    - 버튼 타입 reset은 사용자 경험에 안 좋을 수 있으므로 제거
    - 버튼 ui를 테스트 할 수 있는 story 구현

---
+++ client/src/components/common/button/button.stories.tsx
@@ -0,0 +1,115 @@
+import type { GlobalColors } from '~types/common';
+
+import Image from 'next/image';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import CloseSVG from '../../../../public/svgs/close.svg';
+import ChevronLeftSVG from '../../../../public/svgs/chevron-left.svg';
+import Button from './button';
+
+export default {
+  title: 'common/button',
+  component: Button,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          margin: '3rem',
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '1rem',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Button>;
+
+const BUTTON_COLORS: GlobalColors[] = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'light',
+  'gray',
+];
+
+const Template: ComponentStory<typeof Button> = ({ children, ...args }) => (
+  <>
+    {BUTTON_COLORS.map((color) => (
+      <Button key={null} color={color} {...args}>
+        {children}
+      </Button>
+    ))}
+  </>
+);
+
+export const FillButtonXS = Template.bind({});
+FillButtonXS.args = {
+  children: 'Button',
+  size: 'x-small',
+};
+
+export const FillButtonSM = Template.bind({});
+FillButtonSM.args = {
+  children: 'Button',
+  size: 'small',
+};
+
+export const FillButtonMD = Template.bind({});
+FillButtonMD.args = {
+  children: 'Button',
+  size: 'medium',
+};
+
+export const FillButtonLG = Template.bind({});
+FillButtonLG.args = {
+  children: 'Button',
+  size: 'large',
+};
+
+export const FillButtonXL = Template.bind({});
+FillButtonXL.args = {
+  children: 'Button',
+  size: 'x-large',
+};
+
+export const OutlineButton = Template.bind({});
+OutlineButton.args = {
+  children: 'Button',
+  variant: 'outline',
+};
+
+export const GhostButton = Template.bind({});
+GhostButton.args = {
+  children: 'Button',
+  variant: 'ghost',
+};
+
+export const FullWidthButton = Template.bind({});
+FullWidthButton.args = {
+  children: 'Button',
+  fullWidth: true,
+};
+
+export const IconButtonSM = Template.bind({});
+IconButtonSM.args = {
+  children: <CloseSVG />,
+  size: 'small',
+  variant: 'icon',
+};
+
+export const IconButtonMD = Template.bind({});
+IconButtonMD.args = {
+  children: <ChevronLeftSVG />,
+  size: 'medium',
+  variant: 'icon',
+};
+
+export const IconButtonLG = Template.bind({});
+IconButtonLG.args = {
+  children: <Image src="/images/menu-all.png" width={40} height={40} />,
+  size: 'large',
+  variant: 'icon',
+};
---
+++ client/src/components/common/button/button.tsx
@@ -0,0 +1,61 @@
+import type {
+  ReactNode,
+  ComponentPropsWithoutRef,
+  MouseEventHandler,
+} from 'react';
+import type { ButtonStyle } from './button-base';
+
+import React, { forwardRef } from 'react';
+
+import { FillButton, OutlineButton, GhostButton } from './button-basic';
+import IconButton from './button-icon';
+
+import { useRipple } from '~hooks/index';
+
+type CustomButtonAttirbutes = Omit<
+  ComponentPropsWithoutRef<'button'>,
+  'color' | 'type'
+>;
+type CustomButtonType = 'button' | 'submit'; // 사용자 경험을 위해 'reset' 타입은 사용하지 않습니다!
+
+interface ButtonProps extends CustomButtonAttirbutes, ButtonStyle {
+  type: CustomButtonType;
+  children: ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const BUTTON_VARIANTS = {
+  fill: FillButton,
+  outline: OutlineButton,
+  ghost: GhostButton,
+  icon: IconButton,
+};
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ type, variant, children, onClick, ...rest }, forwardedRef) => {
+    const [addRipple, ripples] = useRipple();
+    const MutableButton = BUTTON_VARIANTS[variant || 'fill'];
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLButtonElement>): void => {
+      addRipple(e);
+    };
+
+    return (
+      <MutableButton
+        ref={forwardedRef}
+        type={type}
+        variant={variant}
+        onClick={onClick}
+        onMouseDown={handleMouseDown}
+        {...rest}
+      >
+        {children}
+        {ripples}
+      </MutableButton>
+    );
+  },
+);
+
+Button.displayName = 'Button';
+
+export default Button;
