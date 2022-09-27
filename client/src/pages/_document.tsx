84395d9e32200a8cc8c37730ea015be29b5cfde5
고동현 <dhjk35@naver.com>
2022-09-27 16:14:21 +0900

    Design: 앱 전체에 폰트 적용(Noto Sans KR, Black Han Sans)

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -6,7 +6,18 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- client/src/styles/global-style.ts
+++ client/src/styles/global-style.ts
@@ -11,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     margin: 0;
     padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-family: 'Noto Sans KR', -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     text-size-adjust: none;
     overflow: hidden;
   }
