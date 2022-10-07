cdf3367d082395e2f94f284dd48368e0fb1513a9
고동현 <dhjk35@naver.com>
2022-10-07 22:14:19 +0900

    Design: hover 시 밑줄을 보여주는 애니메이션 생성

--- client/src/styles/animations.ts
+++ client/src/styles/animations.ts
@@ -1,5 +1,10 @@
 import { css } from 'styled-components';
 
+export interface UnderlineAnimation {
+  color: string;
+  highlight?: string;
+}
+
 export const rippleAnimation = css`
   position: relative;
   overflow: hidden;
@@ -9,5 +14,29 @@ export const rippleAnimation = css`
       transform: scale(15);
       opacity: 0;
     }
+  }
+`;
+
+export const underlineAnimation = css<UnderlineAnimation>`
+  position: relative;
+  display: inline-block;
+  color: ${({ color, highlight }) => highlight || color};
+
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -4px;
+    width: 100%;
+    height: 2px;
+    background-color: ${({ color, highlight }) => highlight || color};
+    transform: scaleX(0);
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+
+  &:hover:after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
   }
 `;
