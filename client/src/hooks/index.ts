280c14e4e50e201064fa92aeb5f3bd989c208021
고동현 <dhjk35@naver.com>
2022-10-20 15:59:06 +0900

    Feat: useInterval 훅 구현 및 테스트

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useRipple } from './use-ripple';
 export { default as useInput } from './use-input';
 export { default as useToggle } from './use-toggle';
 export { default as useStopAnimationOnResize } from './use-stop-animation-on-resize';
+export { default as useInterval } from './use-interval';
