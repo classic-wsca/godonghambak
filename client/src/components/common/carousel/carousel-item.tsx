7af6dad66a029cfc458e943e9ab1c16aacf21a59
고동현 <dhjk35@naver.com>
2022-10-25 22:05:39 +0900

    Feat: Carousel Item 컴포넌트에 현재 슬라이드인 경우의 스타일을 추가할 수 있는 기능 추가

--- client/src/components/common/carousel/carousel-item.tsx
+++ client/src/components/common/carousel/carousel-item.tsx
@@ -1,6 +1,10 @@
-import type { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 
-import styled from 'styled-components';
+import styled, {
+  DefaultTheme,
+  ThemeProps,
+  FlattenInterpolation,
+} from 'styled-components';
 import { pixelToRem } from '~utils/style-utils';
 
 export interface CarouselItemProps extends PropsWithChildren {
@@ -8,6 +12,10 @@ export interface CarouselItemProps extends PropsWithChildren {
   width?: number;
   margin?: number;
   label?: string;
+  currentIndex?: number;
+  slideCount?: number;
+  className?: string; // 스타일 오버라이딩을 위해
+  activeStyle?: FlattenInterpolation<ThemeProps<DefaultTheme>>;
 }
 
 const CarouselItem = ({
@@ -15,13 +23,37 @@ const CarouselItem = ({
   width,
   margin,
   label,
+  currentIndex,
+  slideCount,
+  className, // 스타일 오버라이딩을 위해
+  activeStyle, // active 슬라이드 스타일
   children,
 }: CarouselItemProps) => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (currentIndex === undefined || slideCount === undefined) {
+      return;
+    }
+
+    const currentId = Number(id?.slice(-2)) - 9;
+
+    if (currentId === (currentIndex % slideCount) + 1) {
+      setIsActive(true);
+      return;
+    }
+
+    setIsActive(false);
+  }, [id, currentIndex, slideCount]);
+
   return (
     <CarouselItemWrapper
       id={id}
+      className={className}
       width={width}
       margin={margin}
+      isActive={isActive}
+      activeStyle={activeStyle}
       role="tabpanel"
       aria-roledescription="slide"
       aria-label={label}
@@ -31,7 +63,11 @@ const CarouselItem = ({
   );
 };
 
-const CarouselItemWrapper = styled.li<CarouselItemProps>`
+const CarouselItemWrapper = styled.li<
+  CarouselItemProps & {
+    isActive: boolean;
+  }
+>`
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -40,6 +76,8 @@ const CarouselItemWrapper = styled.li<CarouselItemProps>`
   min-height: ${pixelToRem(200)};
   margin: ${({ margin }) => `0 ${margin}px`};
   user-select: none;
+
+  ${({ isActive, activeStyle }) => isActive && activeStyle};
 `;
 
 export default CarouselItem;
