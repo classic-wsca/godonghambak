4a1101955a62b01b56603b7a7b368acbc1b18ca9
고동현 <dhjk35@naver.com>
2022-11-28 13:29:36 +0900

    test(home-page): 메뉴 소개 섹션 컴포넌트 테스트

---
+++ client/__tests__/components/section/menu-section.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '../../test-utils';
+
+import { MenuSection } from '~components/section';
+
+describe('Menu section component test', () => {
+  it('should be rendered correctly', () => {
+    render(<MenuSection />);
+
+    const section = screen.getByRole('region', { name: /고동함박 메뉴 소개/i });
+
+    expect(section).toBeInTheDocument();
+  });
+});
