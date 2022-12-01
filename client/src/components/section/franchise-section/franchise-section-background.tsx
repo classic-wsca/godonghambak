c39ae3c69e3f61333228fa7a8c7440dd567a3aa8
고동현 <dhjk35@naver.com>
2022-12-01 18:28:16 +0900

    design(FranchiseSection): 캐릭터 이미지 화면 크기에 맞게 크기 변경 적용

--- client/src/components/section/franchise-section/franchise-section-background.tsx
+++ client/src/components/section/franchise-section/franchise-section-background.tsx
@@ -8,6 +8,7 @@ import styled from 'styled-components';
 
 import { CHARACTER_CAROUSEL_ITEMS } from '~constants/carousel';
 import { useThrottle } from '~hooks/index';
+import { pixelToRem } from '~utils/style-utils';
 
 const FranchiseSectionBackground = () => {
   const imageList = [...CHARACTER_CAROUSEL_ITEMS, ...CHARACTER_CAROUSEL_ITEMS];
@@ -58,48 +59,111 @@ const ImageWrapper = styled.li.attrs<{ position: Position }>(
 )<{ position: Position }>`
   position: absolute;
   transition: transform 600ms linear;
+  z-index: 100;
 
   &:nth-of-type(1) {
     top: 60%;
     left: -4%;
-    width: 16vmin;
-    height: 16vmin;
+    width: 12vw;
+    height: 12vw;
   }
 
   &:nth-of-type(2) {
     bottom: -5%;
     left: 10%;
-    width: 30vmin;
-    height: 30vmin;
+    width: 22vw;
+    height: 22vw;
   }
 
   &:nth-of-type(3) {
     top: -8%;
     left: -8%;
-    width: 38vmin;
-    height: 38vmin;
+    width: 28vw;
+    height: 28vw;
   }
 
   &:nth-of-type(4) {
     bottom: 15%;
     right: 2%;
-    width: 22vmin;
-    height: 22vmin;
+    width: 18vw;
+    height: 18vw;
   }
 
   &:nth-of-type(5) {
     top: -5%;
     right: 15%;
-    width: 18vmin;
-    height: 18vmin;
+    width: 14vw;
+    height: 14vw;
   }
 
   &:nth-of-type(6) {
     top: 30%;
-    right: -8%;
-    width: 16vmin;
-    height: 16vmin;
+    right: -4%;
+    width: 10vw;
+    height: 10vw;
   }
+
+  @media screen and (min-width: 1440px) {
+    &:nth-of-type(1) {
+      width: ${pixelToRem(160)};
+      height: ${pixelToRem(160)};
+    }
+
+    &:nth-of-type(2) {
+      width: ${pixelToRem(280)};
+      height: ${pixelToRem(280)};
+    }
+
+    &:nth-of-type(3) {
+      left: -4%;
+      width: ${pixelToRem(360)};
+      height: ${pixelToRem(360)};
+    }
+
+    &:nth-of-type(4) {
+      right: 8%;
+      bottom: 5%;
+      width: ${pixelToRem(320)};
+      height: ${pixelToRem(320)};
+    }
+
+    &:nth-of-type(5) {
+      width: ${pixelToRem(200)};
+      height: ${pixelToRem(200)};
+    }
+
+    &:nth-of-type(6) {
+      right: -4%;
+      width: ${pixelToRem(180)};
+      height: ${pixelToRem(180)};
+    }
+  }
+
+  @media screen and (min-width: 2200px) {
+    &:nth-of-type(1) {
+      left: 4%;
+    }
+
+    &:nth-of-type(2) {
+      left: 24%;
+    }
+
+    &:nth-of-type(3) {
+      left: 12%;
+    }
+
+    &:nth-of-type(4) {
+      right: 16%;
+    }
+
+    &:nth-of-type(5) {
+      right: 22%;
+    }
+
+    &:nth-of-type(6) {
+      right: 4%;
+    }
+  } ;
 `;
 
 export default FranchiseSectionBackground;
