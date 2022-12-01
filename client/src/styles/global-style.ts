e638f2284641e882d12df278e4026399d7778a54
고동현 <dhjk35@naver.com>
2022-12-01 16:58:00 +0900

    fix(godonghambak): body에 overflow-x 때문에 navbar가 상단에 고정(sticky)되지 않는 오류 해결

--- client/src/styles/global-style.ts
+++ client/src/styles/global-style.ts
@@ -12,14 +12,13 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: 'Noto Sans KR', -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    
+    overflow-x: clip;
   }
 
   body {
     width: 100%;
     height: 100%;
     text-size-adjust: none;
-    overflow-x: hidden;
   }
 
   a {
