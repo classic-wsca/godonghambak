073739dfb4ae6e42ee6164075402deb02f43292f
고동현 <dhjk35@naver.com>
2022-12-08 20:54:03 +0900

    feat(validate-utils): 회원가입에 필요한 검증 함수들 구현
    - validateJoinForm: 회원가입 폼의 모든 필드를 검증해 에러 메시지를 담은
      객체를 반환하는 함수
    - validateJoinInput: 회원가입 폼의 각 필드들을 검증하는 함수
    - validateName: 2-15자 이내의 한글로 이루어진 이름인지 검증하는 함수
    - validateBirth: 올바른 생년월일 6자리인지 검증하는 함수
    - isEqual: 입력받은 두 인자가 동일한지 검증하는 함수

--- client/src/utils/validate-utils.ts
+++ client/src/utils/validate-utils.ts
@@ -1,8 +1,52 @@
 import type { LoginInformation } from '~pages/login';
+import type { UserInformation } from '~types/auth';
 
 import { HTMLInputTypeAttribute } from 'react';
 
+import { JOIN_ERROR_MESSAGES } from '~constants/join';
 import { LOGIN_ERROR_MESSAGES } from '~constants/login';
+
+export const validateJoinForm = (
+  fields: UserInformation,
+  verificationCode?: string,
+) => {
+  const errors: Partial<UserInformation> = {};
+
+  Object.entries(fields).forEach(([key, value]) => {
+    const { notExist, invalidFormat } = JOIN_ERROR_MESSAGES;
+    const compareValue =
+      key === 'passwordConfirm' ? fields.password : verificationCode;
+
+    if (!validateJoinInput(value, key, compareValue)) {
+      errors[key] = value === '' ? notExist[key] : invalidFormat[key];
+    }
+  });
+
+  return errors;
+};
+
+export const validateJoinInput = (
+  value: string,
+  name: keyof UserInformation,
+  compareValue?: string,
+) => {
+  const validate: {
+    [key: keyof UserInformation]: (
+      value: string,
+      compareValue?: string,
+    ) => boolean;
+  } = {
+    email: validateEmail,
+    emailVerificationCode: isEqual,
+    password: validatePassword,
+    passwordConfirm: isEqual,
+    phoneNumber: validatePhoneNumber,
+    name: validateName,
+    birth: validateBirth,
+  };
+
+  return validate[name](value, compareValue);
+};
 
 export const validateLogin = ({ email, password }: LoginInformation) => {
   const errors: Partial<LoginInformation> = {};
@@ -38,6 +82,10 @@ export const validateInput = (
     return validatePhoneNumber(value);
   }
 
+  if (type === 'name') {
+    return validateName(value);
+  }
+
   return null;
 };
 
@@ -46,9 +94,14 @@ export const validateEmail = (value: string) => {
   return regex.test(value);
 };
 
-export const validatePassword = (value: string): boolean | null => {
+export const validatePassword = (value: string) => {
   // 길이가 7-15 사이의 하나 이상의 숫자와 특수 문자를 가진 문자열
   const regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+  return regex.test(value);
+};
+
+export const validateName = (value: string) => {
+  const regex = /^[가-힣]{2,15}$/;
   return regex.test(value);
 };
 
@@ -56,4 +109,33 @@ export const validatePhoneNumber = (value: string) => {
   // 01[016789]-000[0]-0000
   const regex = /^01[016789]{1}?[- ]?(\d{3,4})[- ]?(\d{4})$/;
   return regex.test(value);
+};
+
+export const validateBirth = (value: string) => {
+  if (value.length !== 6) {
+    return false;
+  }
+
+  const regex = /([0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[1,2][0-9]|3[0,1]))/;
+
+  if (!regex.test(value)) {
+    return false;
+  }
+
+  const month = value.slice(2, 4);
+  const date = value.slice(4);
+
+  if (/^((0?[469])|11)$/.test(month) && date === '31') {
+    return false;
+  }
+
+  if (parseInt(month, 10) === 2 && parseInt(date, 10) > 29) {
+    return false;
+  }
+
+  return true;
+};
+
+export const isEqual = <T>(valueA: T, valueB: T) => {
+  return valueA === valueB;
 };
