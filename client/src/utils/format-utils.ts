85cc0f016c7f9a5e65f3c60428d2d07fff7a2667
고동현 <dhjk35@naver.com>
2022-12-10 16:58:31 +0900

    feat(convertNumberToTime): 숫자를 일, 시간, 분, 초 단위로 변경해주는 함수 구현

--- client/src/utils/format-utils.ts
+++ client/src/utils/format-utils.ts
@@ -1,3 +1,37 @@
+const TIME_UNIT = Object.freeze({
+  second: 60,
+  minute: 60,
+  hour: 24,
+});
+
+const TIME = Object.freeze({
+  minute: 60,
+  hour: 60 * 60,
+  day: 24 * 60 * 60,
+});
+
+export const convertNumberToTime = (time: number) => {
+  if (time < 0) {
+    throw new RangeError('0보다 작은 수는 시간으로 환산할 수 없습니다.');
+  }
+
+  if (time > Number.MAX_SAFE_INTEGER) {
+    throw new RangeError('시간으로 환산할 수 있는 범위를 벗어났습니다.');
+  }
+
+  const days = Math.floor(time / TIME.day);
+  const hours = Math.floor((time / TIME.hour) % TIME_UNIT.hour);
+  const minutes = Math.floor((time / TIME_UNIT.minute) % TIME_UNIT.minute);
+  const seconds = Math.floor((time % TIME_UNIT.second) % TIME_UNIT.second);
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+};
+
 export const formatPhoneNumber = (value: string): string => {
   if (!value) return '';
   const phoneNumber = value.replace(/[^\d]/g, '');
